c3d890ee6ceba297fa43aec8c7ca5980e66678d5
Peter Seidl <39053630+InterCrone@users.noreply.github.com>
2018-06-11 14:02:07 +0200

    Port Change

--- src/bitcoinrpc.cpp
+++ src/bitcoinrpc.cpp
@@ -42,7 +42,7 @@ void ThreadRPCServer3(void* parg);
 
 static inline unsigned short GetDefaultRPCPort()
 {
-    return GetBoolArg("-testnet", false) ? 21655 : 47916;
+    return GetBoolArg("-testnet", false) ? 6602 : 6601;
 }
 
 Object JSONRPCError(int code, const string& message)
